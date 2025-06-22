fc4480436616486dcafff2db677aa2e885cc2920
who am i..? <49607060+NamasteIndia@users.noreply.github.com>
2025-06-23 00:23:21 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -1,15 +1,15 @@
-import asyncio
+from asyncio import sleep
 from secrets import token_hex
-from telegraph.aio import Telegraph as TelegraphApi
+from telegraph.aio import Telegraph
 from telegraph.exceptions import RetryAfterError
 
 from ... import LOGGER
 from ...core.config_manager import Config
 
 
-class Telegraph:
+class TelegraphHelper:
     def __init__(self, author_name=None, author_url=None):
-        self._telegraph = TelegraphApi(domain="graph.org")
+        self._telegraph = Telegraph(domain="graph.org")
         self._author_name = author_name
         self._author_url = author_url
 
@@ -34,9 +34,9 @@ class Telegraph:
             )
         except RetryAfterError as st:
             LOGGER.warning(
-                f"Telegraph Flood control exceeded. Sleeping for {st.retry_after} seconds."
+                f"Telegraph Flood control exceeded. I will sleep for {st.retry_after} seconds."
             )
-            await asyncio.sleep(st.retry_after)
+            await sleep(st.retry_after)
             return await self.create_page(title, content)
 
     async def edit_page(self, path, title, content):
@@ -50,56 +50,36 @@ class Telegraph:
             )
         except RetryAfterError as st:
             LOGGER.warning(
-                f"Telegraph Flood control exceeded. Sleeping for {st.retry_after} seconds."
+                f"Telegraph Flood control exceeded. I will sleep for {st.retry_after} seconds."
             )
-            await asyncio.sleep(st.retry_after)
+            await sleep(st.retry_after)
             return await self.edit_page(path, title, content)
 
-    async def edit_telegraph(self, paths, telegraph_contents):
-        """
-        Edit multiple Telegraph pages with navigation links (Prev | Next).
-        """
-        num_of_paths = len(paths)
-        for index, content in enumerate(telegraph_contents):
-            navigation_links = []
-            if index > 0:
-                prev_link = f'<b><a href="https://telegra.ph/{paths[index - 1]}">Prev</a></b>'
-                navigation_links.append(prev_link)
-            if index < num_of_paths - 1:
-                next_link = f'<b><a href="https://telegra.ph/{paths[index + 1]}">Next</a></b>'
-                navigation_links.append(next_link)
-            if navigation_links:
-                content += " | ".join(navigation_links)
+    async def edit_telegraph(self, path, telegraph_content):
+        nxt_page = 1
+        prev_page = 0
+        num_of_path = len(path)
+        for content in telegraph_content:
+            if nxt_page == 1:
+                content += (
+                    f'<b><a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
+                )
+                nxt_page += 1
+            else:
+                if prev_page <= num_of_path:
+                    content += f'<b><a href="https://telegra.ph/{path[prev_page]}">Prev</a></b>'
+                    prev_page += 1
+                if nxt_page < num_of_path:
+                    content += f'<b> | <a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
+                    nxt_page += 1
             await self.edit_page(
-                path=paths[index], title="WZML-X Torrent Search", content=content
+                path=path[prev_page],
+                title="WZML-X Torrent Search",
+                content=content,
             )
-
-    async def hibernate(self):
-        LOGGER.info("Telegraph: Suspending service...")
-        await self.suspend()
-
-    async def shutdown(self):
-        LOGGER.info("Telegraph: Shutting down service...")
-        await self.stop()
-
-    async def suspend(self):
-        # Add real suspension logic here if applicable
-        await asyncio.sleep(0.1)
-
-    async def stop(self):
-        # Add real shutdown logic here if applicable
-        await asyncio.sleep(0.1)
+        return
 
 
-telegraph = Telegraph(Config.AUTHOR_NAME, Config.AUTHOR_URL)
+telegraph = TelegraphHelper(Config.AUTHOR_NAME, Config.AUTHOR_URL)
 
-
-if __name__ == "__main__":
-    import asyncio
-
-    async def test():
-        await telegraph.create_account()
-        page = await telegraph.create_page("Test Title", "<p>Test Content</p>")
-        print(f"Created page: https://telegra.ph/{page['path']}")
-
-    asyncio.run(test())
+print(__name__)
